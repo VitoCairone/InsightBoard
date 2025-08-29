bcd509c47f60905c8f56270f282d201f6c5c9452
Vito Cairone <vitocairone@gmail.com>
2025-08-29 14:01:21 -0500

    setup and run prisma api express backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-
+import { PrismaClient } from '@prisma/client'
 const app = express();
+const prisma = new PrismaClient();
 const PORT = process.env.PORT || 4000;
 
-app.get('/api/hello', (_req, res) => {
-  res.json({ message: 'Hello from InsightBoard backend!' });
+app.get('/api/hello', async (_req, res) => {
+  const users = await prisma.user.findMany();
+  res.json({ message: 'Hello from InsightBoard backend!', users });
 });
 
 app.listen(PORT, () => {
